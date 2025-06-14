c5033a8e38878ad1d715809e31cc46c52b0555d0
LucasCoder6969 <DeveloperLucas2025@hotmail.com>
2025-06-14 17:22:17 -0300

    Source Code

---
+++ BetterTerminal.cmd
@@ -0,0 +1,40 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJhZksaHErSXA==
+::ZQ05rAF9IBncCkqN+0xwdVsFAlTMbCXqZg==
+::ZQ05rAF9IAHYFVzEqQISLQ9ASRbCCG6oC7wS6eSb
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDCaHKH+/FPUo7fr25uSGri0=
+::cRolqwZ3JBvQF1fEqQISLQ9ASRbCCG6oC7wS6eSb
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATEphJifXs=
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFBdBRRfRAES0A5EO4f7+00lmsEgcFOY6OLvJ27aPLOgc1mfhYYIoxDpundkBBRVZclyudgpU
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+title BetterTerminal - 1.0
+chcp 65001 > nul
+:main
+set /p command="%cd% $ "
+%command%
+goto main
